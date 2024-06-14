7a0ac7f7c633e7db15c5c8a8be0b1a1f39f7fee7
Yugo <dedy.yugo.purwanto@gmail.com>
2024-06-14 15:18:56 +0700

    Test for throttle and transaction summary

--- tests/Feature/Api/V1/PaymentControllerTest.php
+++ tests/Feature/Api/V1/PaymentControllerTest.php
@@ -5,6 +5,7 @@ use App\Models\Enums\TransactionStatus;
 use App\Models\Transaction;
 use App\Models\User;
 use Illuminate\Support\Arr;
+use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Queue;
 use Symfony\Component\HttpFoundation\Response;
 
@@ -12,6 +13,20 @@ use function Pest\Laravel\assertDatabaseHas;
 
 beforeEach(function (): void {
     $this->user = User::factory()->create();
+});
+
+it('throttles payment endpoint', function (): void {
+    for ($i = 1; $i <= 60; ++$i) {
+        $this->actingAs($this->user,'api')->getJson('/api/v1/payment');
+    }
+
+    $response = $this->actingAs($this->user, 'api')
+        ->getJson('/api/v1/payment');
+
+    $response->assertStatus(Response::HTTP_TOO_MANY_REQUESTS)
+        ->assertJson([
+            'message' => 'Too Many Attempts.',
+        ]);
 });
 
 it('authenticates user before make a payment', function (): void {
@@ -98,14 +113,81 @@ it('returns list of transaction for authenticated user', function (): void {
         ]);
 });
 
+it('authenticates to access transaction summary', function (): void {
+    $this->getJson('/api/v1/payment/summary')
+        ->assertStatus(Response::HTTP_UNAUTHORIZED)
+        ->assertJson([
+            'message' => 'Unauthenticated.',
+        ]);
+});
+
 it('calculates summary for all transactions', function (): void {
-    $this->artisan('db:table:truncate');
+    // clean table before make a calculation for summary
+    Transaction::query()->truncate();
 
-    $transactions = Transaction::factory()
+    $transactions = Transaction::factory($count = 2)
         ->for($this->user)
-        ->create();
+        ->create([
+            'status' => TransactionStatus::Completed,
+        ]);
 
-    assertDatabaseHas((new Transaction)->getTable(), [
-        'id' => $transactions->id,
-    ]);
+    $response = $this->actingAs($this->user, 'api')
+        ->getJson('/api/v1/payment/summary');
+
+    $maxAmount = $transactions->where('status', value: TransactionStatus::Completed)
+        ->max('amount');
+
+    $minAmount = $transactions->where('status', value: TransactionStatus::Completed)
+        ->min('amount');
+
+    $highest = $transactions->firstWhere(function (Transaction $transaction) use ($maxAmount): bool {
+        return $transaction->status === TransactionStatus::Completed
+            && $transaction->amount >= $maxAmount;
+    });
+
+    $lowest = $transactions->firstWhere(function (Transaction $transaction) use ($minAmount): bool {
+        return $transaction->status === TransactionStatus::Completed
+            && $transaction->amount >= $minAmount;
+    });
+
+    $maxName = $transactions->max(fn(Transaction $transaction): int => strlen($transaction->user->name));
+
+    $longest = $transactions
+        ->firstWhere(fn(Transaction $transaction): bool => strlen($transaction->user->name) === $maxName);
+
+    $response->assertOk()
+        ->assertJson([
+            'data' => [
+                'total_transaction' => $count,
+
+                'average_amount' => round($transactions->avg('amount'), 2),
+
+                'highest_transaction' => [
+                    'id' => $highest->id,
+                    'user_id' => $highest->user_id,
+                    'amount' => $highest->amount,
+                    'status' => $highest->status->value,
+                ],
+
+                'lowest_transaction' => [
+                    'id' => $lowest->id,
+                    'user_id' => $lowest->user_id,
+                    'amount' => $lowest->amount,
+                    'status' => $lowest->status->value,
+                ],
+
+                'longest_name_transaction' => [
+                    'id' => $longest->id,
+                    'user_id' => $longest->user_id,
+                    'amount' => $longest->amount,
+                    'status' => $longest->status->value,
+                ],
+
+                'status_distribution' => $transactions->groupBy('status')
+                    ->mapWithKeys(function (Collection $transactions, string $status): array {
+                        return [$status => $transactions->count()];
+                    })
+                    ->toArray(),
+            ]
+        ]);
 });
